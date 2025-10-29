270416d3cfdc4d666be91e5e5bc5a3facb943e7c
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2025-10-29 19:22:45 +0530

    Update Graph.c

--- Graph/Graph.c
+++ Graph/Graph.c
@@ -21,6 +21,11 @@ void intiGraph(){
         }
     }
 }
+// Adding Edge
+void addEdge(int u,int v,int directed) {
+    adj[u][v]=1; // For Directed because it only follows 1 way connection
+    if(!directed) adj[v][u]=1; // undirected
+}
 // Displaying the Adjaceny Matrix
 void displayGraph(){
     for(int i=0;i<n;i++){
@@ -50,7 +55,55 @@ void addEdgeList(int u,int v,int directed){
         adjList[v] = newNode;
     }
 }
-
+// Displaying Adjacency List
+void displayList(int n){
+    for(int i=0;i<n;i++){
+        printf("%d: ",i);
+        struct Node* temp = adjList[i];
+        while(temp){
+            printf("%d ",temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+// BFS using adjacency Matrix
+void BFS(int start){
+    int visited[10] = {0};
+    int queue[10], front = 0, rear = 0; // Queue stores the Node's index
+    visited[start] = 1;
+    queue[rear++] = start; 
+    while(front < rear){
+        int u = queue[front++];
+        printf("%d ",u);
+        for(int v=0; v<n;v++){
+            if(adj[u][v] && !visited[v]){
+                visited[v] = 1;
+                queue[rear++] = v;
+            }
+        }
+    }
+    printf("\n");
+    // TC: O(n^2) , SC: O(n)
+   // Can be optimized with Adjacency List
+}
+// DFS using Adjacency Matrix
+void DFS(int start){
+    int visited[10] = {0};
+    DFSHelper(start,visited);
+    printf("\n");
+}
+void DFSHelper(int u,int visited[]){
+    visited[u] = 1;
+    printf("%d ",u);
+    for(int v=0;v<n;v++){
+        if(adj[u][v] && !visited[v]){
+            DFSHelper(v,visited);
+        }
+    }
+    // TC: O(n^2) , SC: O(n); 
+   // Can be optimized with Adjacency List
+}
 int main(){
     printf("Author: Divyansh Garg , Starman248");
     return 0;
